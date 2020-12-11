8b968b8fabc3af8a970fdbef33f6578c7e74cea4
Gavin den hollander <gavindhollander@gmail.com>
2020-12-11 16:02:42 +0100

    woops

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import React, {useRef} from 'react';
-import { Section, Button, Grid, Avatar, Row, Col, Timeline, Pictogram, Wave} from "./components/components"
+import { Section, Button, Grid, Avatar, Row, Col, Timeline, Pictogram, Wave, ImageHover, ImageModel} from "./components/components"
 //import Section from "./components/section/index";
 import "./app.scss"
 
-
 const scroll = (ref:any) => window.scrollTo(0, ref.current.offsetTop)
 
-const test = ["1", "2","3", "4"];
-const pictoIcons: string[] = [
-  "https://cdn.iconscout.com/icon/free/png-256/html5-40-1175193.png",
-"https://freeiconshop.com/wp-content/uploads/edd/html-outline.png",
-"https://cdn.iconscout.com/icon/free/png-256/css-37-226088.png",
-"https://cdn.iconscout.com/icon/free/png-256/sass-226054.png",
-"https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/13691885491579517854-256.png",
-"https://cdn.iconscout.com/icon/free/png-256/typescript-1174965.png",
-"https://cdn.iconscout.com/icon/free/png-256/java-43-569305.png",
-"https://icons.iconarchive.com/icons/cornmanthe3rd/plex/256/Other-python-icon.png",
-"https://cdn.iconscout.com/icon/free/png-256/php-2038871-1720084.png",
-"https://lh3.googleusercontent.com/proxy/N1wt76Wj8xuUP3lBYRSTFkJ6cHlltAHBigcyHINE_XjxpR6AX_lR7c6mALHy10DnDehpoE12bzQ1WCP-_QF1doj7NEOjTKD4VtKkh9d6Whb7XxF1sAtZp13NhbLsxrsQU2k",
-"https://cdn.iconscout.com/icon/free/png-256/c-sharp-3-599433.png",
+const lang: string[] = [
+  "js", "csharp", "css", "html", "html5", "java", "php", "python", "sass", "sql", "typescript"
+];
+const frameworks: string[] = [
+  "antd", "hibernate", "materialui", "processing", "react", "react_native", "spring",
 ];
 
-const pictoIcons2: string[] = [
-  "https://camo.githubusercontent.com/a25ae4eec22b44056ec7471a62893673fcd0dc99/68747470733a2f2f66696c6573746f72652e6861737572612e696f2f76312f66696c652f37373233633233342d343236332d343539392d393536382d396436373838313338633562",
-  "https://images.opencollective.com/ant-design/6f1eb50/logo/256.png",
-  "https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/10218066661551941702-256.png",
-  "https://cdn2.iconfinder.com/data/icons/designer-skills/128/react-256.png",
-  "https://miro.medium.com/max/512/1*k73wp-nDid53eeQ0RDGvdw.png",
-  "https://lh3.googleusercontent.com/proxy/K4uDyP-PM9B-aPbNfIILJK_T7-EYFflrzZAVn8W8lAdi7QMmyygARMtG3qLEioN7gJvEfwc_gkyEsf3I-RQS9g",
-  "https://tools.jboss.org/features/images/hibernate_icon_256px.png",
-
+const programs: string[] = [
+  "autocad", "git", "intelij", "office", "photoshop", "rhinoceros", "vscode"
 ];
 
 function App() {
@@ -85,18 +69,21 @@ function App() {
         <Row center>
           <Col size={5} scale="xl">
             <h1>Over Mij</h1>
-            Mijn naam is Gavin den Hollander, 23 jaar, en ik woon in
-            Zwijndrecht. Vanaf kinds af aan ben ik altijd nieuwsgierig en
-            leergierig geweest. Hierdoor wilde ik altijd weten hoe iets werkt en
-            hoe dingen gemaakt worden. Dit geldt ook voor websites en met veel
-            zoeken en proberen heb ik op mijn 14de mijn eerste website gemaakt.
-            Hij was niet mooi of goed geschreven maar wat was ik trots. Vanaf
-            dat moment ben ik altijd bezig geweest met programmeren. Omdat ik
-            programmeren voornamelijk als een hobby zag ben ik op mijn 17de de
-            opleiding Scheeps- & Jachtbouw gaan volgen op het STC in Rotterdam.
-            Eenmaal in mijn tweede jaar ben ik erachter gekomen dat dit toch
-            niet mijn ding was. Alsnog heb ik de opleiding afgemaakt en ben ik
-            nu bezig met de opleiding informatica aan de Hogeschool Rotterdam.{" "}
+            <div>
+              Mijn naam is Gavin den Hollander, 23 jaar, en ik woon in
+              Zwijndrecht. Vanaf kinds af aan ben ik altijd nieuwsgierig en
+              leergierig geweest. Hierdoor wilde ik altijd weten hoe iets werkt en
+              hoe dingen gemaakt worden. Dit geldt ook voor websites en met veel
+              zoeken en proberen heb ik op mijn 14de mijn eerste website gemaakt.
+              Hij was niet mooi of goed geschreven maar wat was ik trots. Vanaf
+              dat moment ben ik altijd bezig geweest met programmeren. Omdat ik
+              programmeren voornamelijk als een hobby zag ben ik op mijn 17de de
+              opleiding Scheeps- & Jachtbouw gaan volgen op het STC in Rotterdam.
+              Eenmaal in mijn tweede jaar ben ik erachter gekomen dat dit toch
+              niet mijn ding was. Alsnog heb ik de opleiding afgemaakt en ben ik
+              nu bezig met de opleiding informatica aan de Hogeschool Rotterdam.
+            </div>
+
           </Col>
           <Col size={3} style={{ maxHeight: 200 }} addClass="hide-1290">
             <Timeline />
@@ -111,14 +98,20 @@ function App() {
       >
         <Row center>
           <Col size={5} scale="xl" addClass="align-items">
+            <h1 className="left no-padding">Ervaring</h1>
             <div>
-              {pictoIcons.map((i) => (
-                <Pictogram url={i} />
+              {lang.map((i) => (
+                <Pictogram url={require(`./images/lang/${i}.png`)} />
               ))}
             </div>
             <div>
-              {pictoIcons2.map((i) => (
-                <Pictogram url={i} />
+              {frameworks.map((i) => (
+                <Pictogram url={require(`./images/frameworksOrLibs/${i}.png`)} />
+              ))}
+            </div>
+            <div>
+              {programs.map((i) => (
+                <Pictogram url={require(`./images/programs/${i}.png`)} />
               ))}
             </div>
           </Col>
@@ -127,27 +120,93 @@ function App() {
           </Col>
         </Row>
       </Section>
-      <Section type="purple" style={{ minHeight: 200 }} refProp={refCV}>
-        CV
+      <Section type="purple" style={{ minHeight: 200, paddingTop:50 }} refProp={refCV}>
+        <Row center>
+          <Col size={5} scale="xl" addClass="align-items hide-1290">
+            <h1 className="left">Curriculum vitae</h1>
+            <ImageHover image={require("./images/quintor.jpeg")}>Minor Fullstack Java</ImageHover>
+            <ImageHover image={require("./images/cofano.jpg")}>3de jaars Stage Cofano software solutions</ImageHover>
+            <ImageHover image={require("./images/printing.jpg")}>Eigenaar CT Innovations</ImageHover>
+            <ImageHover image={require("./images/hr.jpeg")}>Start HBO Informatica</ImageHover>
+          </Col>
+          <Col size={3} scale="xl">
+            <ImageModel
+            src={require("./images/cv.png")} imageStyle={{ width: '100%' }} alt="CV" />
+          </Col>
+        </Row>
+       
       </Section>
+      <Wave color="f7f7f7" bgColor="840bc0"/>
       <Section type="off-white" refProp={refProjects}>
-        Projecten
+        <Row center>
+          <Col size={8} scale="xl" addClass="align-items">
+            <h1 className="left no-padding">Projecten</h1>
+            <Row style={{padding:0,margin:0}}>
+              <Col size={6} scale="xl" addClass="align-items">
+                <ImageHover image={require("./images/project-d.png")}>
+                  <p>Project d had als doel kijken of het mogelijk is de impuls aankopen in een winkel te vergroten,</p>
+                  <p>Hiervoor hebben we een systeem gemaakt wat mensen in een winkel trackt en omvormt in een heatmap.</p>
+                  <p>Dit maakt het mogelijk de beste plekken voor producten in een winkel te zien.</p>
+                  <p>uiteindelijke hebben we ook eye tracking toegevoegd zodat we de meest bekeken producten in een schap kunnen zien.</p>
+                  <p>Gebruikte technieken:</p>
+                  <p>Python voor human detectie an eye tracking</p>
+                  <p> - OpenCV</p>
+                  <p> - Yolo v3 (CPU)</p>
+                  <p> - Dlib</p>
+                  <p>PHP voor de verbinding met de database</p>
+                  <p>ReactJS</p>
+                  <p> - Ant design</p>
+
+
+                  </ImageHover>
+
+              </Col>
+              <Col size={6} scale="xl">
+                <ImageHover image={require("./images/cloudsourced.jpeg")}>
+                  <p>Minor Quintor</p>
+                  <p>Eind jaar 3 heb ik de minor fullstack java developer gevold.</p>
+                  <p>In dit project hebben we een component outsource app gemaakt.</p>
+                  <p>De frontend is gemaakt in React en de backend in Java</p>
+                  <p>Tijdens dit project is er ook een hoop aandacht geweest voor testen</p>
+                  <p>Gebruikte technieken:</p>
+                  <p>React</p>
+                  <p> - Typescript</p>
+                  <p> - Ant Design</p>
+                  <p>Java</p>
+                  <p> - Spring Boot</p>
+                  <p> - PostGres</p>
+                  <p> - Hibernate</p>
+                  <p> - Lombok</p>
+                </ImageHover>
+              </Col>
+              <Col size={12} scale="xl">
+                <ImageHover image={require("./images/quintor.jpeg")}>
+                  <p>Portfolio</p>
+                  <p>Dit portfolio is ook een van mijn laatste projecten.</p>
+                  <p>Voor dit project heb ik alleen gebruik gemaakt van typescript, React en Sass</p>
+                  <p>Ik heb expres geen externe libareries gebruikt voor componenten en styling omdat ik het een leuke uitdaging vond alles zelf te maken.</p>
+                  <p>Gebruikte technieken:</p>
+                  <p>React</p>
+                  <p> - Typescript</p>
+                  <p> - scss</p>
+                </ImageHover>
+              </Col>
+
+            </Row>
+
+          </Col>
+        </Row>
+        
       </Section>
-      <Section type="dark" style={{ minHeight: 200 }} refProp={refContanct}>
-        <p>
-          Telefoon : <a href="tel:+31651969839">+31651969839</a>
-        </p>
-        <p>
-          WhatsApp :{" "}
-          <a href="https://wa.me/0031651969839">
-            +31651969839
-          </a>
-        </p>
-        <p>
-          E-mail :{" "}
-          <a href="mail:Gavindhollander@gmail.com">Gavindhollander@gmail.com</a>
-        </p>
-      </Section>
+      <Section type="dark" refProp={refContanct}>
+        <Row center>
+          <a href="tel:+31651969839" ><Pictogram url={require(`./images/icon/phone.png`)} width={30} /></a>
+          <a href="https://wa.me/0031651969839"><Pictogram url={require(`./images/icon/whatsapp.png`)} width={30} /></a>
+          <a href="mailto:gavindhollander@gmail.com" ><Pictogram url={require(`./images/icon/email.png`)} width={30} /></a>
+          <a href="#" onClick={() => window.open("https://www.linkedin.com/in/gavin-den-hollander-06a96214a", "_blank")}><Pictogram url={require(`./images/icon/linkedin.png`)} width={30} /></a>
+
+        </Row>
+</Section>
     </div>
   );
 }
